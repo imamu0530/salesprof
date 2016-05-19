ebe46c4cb09a4035802289def668b0dfa431dfe3
imamu0530 <imamura@eswell.co.jp>
2016-05-19 12:42:46 +0000

    kadai_syusei

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,13 +34,12 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
-    haves.create(item_id: item.id)
-    #haves.find_or_create_by(item_id: item.id)
+    #haves.create(item_id: item.id)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
     haves.find_by(item_id: item.id).destroy
-    #haves.find_by(item_id: item.id).destroy
   end
 
   def have?(item)
@@ -48,8 +47,8 @@ class User < ActiveRecord::Base
   end
 
   def want(item)
-    wants.create(item_id: item.id)
-    #wants.find_or_create_by(item_id: item.id)
+    #wants.create(item_id: item.id)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
